17ad562777cca49587ad4c211372101703cf7158
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-03-21 23:19:56 -0400

    Cleaned up code

--- markupCalculator.js
+++ markupCalculator.js
@@ -1,39 +1,28 @@
 /** Markup Calculator **/
 
-// User story 1: Without exception, there is a flat markup on all jobs of 5% -> basePrice * 1.05
-// User story 2: For each person that needs to work on the job, there is a markup of 1.2%  -> basePrice * 1.05 * 1.012 * numOfWorkers
-// User story 3: Markups are also added depending on the types of materials involved:
-//
-// * If pharmaceuticals are involved, there is an immediate 7.5% markup
-// * For food, there is a 13% markup
-// * Electronics require a 2% markup
-// * Everything else, there is no markup
-
-// User story 4: The markup calculator should accept the (1)initial base price (2) along with the different categories of markups and calculate a final cost for a project.
-
 var basePrice = Number(process.argv[2]);
 var numOfWorker = Number(process.argv[3]);
 var field = process.argv[4];
 
 function markupCalculator (basePrice, numOfWorker, field) {
-	var flatRate = 5;
-  var labourRate = 1.2;
+	var flatRate = 0.05;
+  var labourRate = 0.012;
 	var addonRate;
 
 	// Without exception, there is a flat markup on all jobs of 5%
-	var flatMarkup = roundNum(basePrice * (100 + flatRate) / 100); // return 13079.8
+	var flatMarkup = roundNum(basePrice * flatRate);
 
   // If pharmaceuticals are involved, there is an immediate 7.5% markup
 	// For food, there is a 13% markup
 	// Electronics require a 2% markup
 	// Everything else, there is no markup
-	if (field === "drugs"){ addonRate = 7.5; }
-	else if (field === "food"){ addonRate = 13; }
-	else if (field === "electronics"){ addonRate = 2; }
+	if (field === "drugs"){ addonRate = 0.075; }
+	else if (field === "food"){ addonRate = 0.13; }
+	else if (field === "electronics"){ addonRate = 0.02; }
 	else { addonRate = 0; }
 
 	// For each person that needs to work on the job, there is a markup of 1.2%
-	var result = roundNum(flatMarkup * (100 + numOfWorker * labourRate * addonRate) / 100);
+	var result = roundNum((basePrice + flatMarkup) * (1 + numOfWorker * labourRate * addonRate));
 
 	console.log(result);
 };
